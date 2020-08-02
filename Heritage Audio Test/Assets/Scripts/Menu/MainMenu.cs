9660ea1234f01e1bcd9265f9036252607e4f7460
Avik Chari <60371063+avikchari@users.noreply.github.com>
2020-08-02 15:30:07 +0800

    Added Audio Test Project

---
+++ Heritage Audio Test/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator CrossFade;
+    public float transitionTime = 1f;
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        CrossFade.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+   
+}
