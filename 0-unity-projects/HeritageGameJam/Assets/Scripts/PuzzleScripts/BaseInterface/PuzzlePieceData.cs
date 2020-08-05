5a29e3c1e4b7129c0f18d7847525f7aef6b62847
ChongJunYi9009 <56516269+ChongJunYi9009@users.noreply.github.com>
2020-08-06 07:32:18 +0800

    Imported
    -Shaders
    -Postprocessing

--- 0-unity-projects/HeritageGameJam/Assets/Scripts/PuzzleScripts/BaseInterface/PuzzlePieceData.cs
+++ 0-unity-projects/HeritageGameJam/Assets/Scripts/PuzzleScripts/BaseInterface/PuzzlePieceData.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class PuzzlePieceData
 {
     public GameObject puzzlePieceObj;
-    public enum puzzleTypeEnum { matchToExactLocation };
+    public enum puzzleTypeEnum { matchToExactLocation, matchShape };
     public puzzleTypeEnum puzzleType;
     public bool successState;
 }
---
+++ 0-unity-projects/HeritageGameJam/Assets/Scripts/PuzzleScripts/PuzzleMaster_2_Conversation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleMaster_2_Conversation : PuzzleMaster_Base
+{
+    public int numOfPlayerConversations;
+    public List<PuzzlePieceData> puzzleManagement;
+    public bool overallSolved = false;
+    public GameObject nextSceneObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void RecievePuzzle_Succeed(bool successState, GameObject source)
+    {
+        bool letsBeOptimisic = true;
+        foreach (PuzzlePieceData data in puzzleManagement)
+        {
+            if(source == data.puzzlePieceObj)
+            {
+                data.successState = successState;
+            }
+
+            //Puzzle is solved only if all pieces are solved
+            if (letsBeOptimisic && data.successState == false) letsBeOptimisic = false;
+        }
+
+        if (letsBeOptimisic)
+        {
+            overallSolved = true;
+            nextSceneObj.SetActive(true);
+            Debug.Log("Yay You finish the puzzle");
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+    }
+}
