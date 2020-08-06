d38016fade116fc10b72d16fc596c71eb56c3b30
Avik Chari <60371063+avikchari@users.noreply.github.com>
2020-08-06 17:07:50 +0800

    Added Menu Music + Script for Menu Music

---
+++ 1-assets/2-code/AudioIntroLoop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class AudioIntroLoop : MonoBehaviour
+{
+    public AudioSource Intro;
+    public AudioSource Loop;
+    public AudioClip introClip;
+    public AudioClip loopClip;
+
+    public float introClipTime = 4.8f;
+
+    void Start()
+    {
+        Loop.loop = true;
+        StartCoroutine(playMenuMusic());
+    }
+ 
+    IEnumerator playMenuMusic()
+    {
+        Intro.clip = introClip;
+        Intro.Play();
+        yield return new WaitForSeconds(introClipTime);
+        Loop.clip = loopClip;
+        Loop.Play();
+    }
+}
